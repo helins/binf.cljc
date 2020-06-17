5ae5d1cb24843c08b340c1c38482c81be29c994c
Adam Helinski <adam@dvlopt.io>
2020-06-17 19:43:08 +0200

    Improve config for CLJS dev

---
+++ dev/cljs/user.cljs
@@ -0,0 +1,16 @@
+(ns cljs.user
+
+  "For daydreaming in the repl."
+
+  (:require [dvlopt.binf      :as binf]
+            [dvlopt.binf-test :as binf-test]))
+
+
+;;;;;;;;;;
+
+
+(comment
+
+
+
+  )
